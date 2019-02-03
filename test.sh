12ea6ef723e55b58aea9522f4765293fd47ae3d4
Israel Levin <israel@paket.global>
2019-02-03 15:17:23 +0200

    Randomer test

--- test.sh
+++ test.sh
@@ -1,43 +1,46 @@
 #!/bin/bash
-#cleos wallet unlock < ~/eos/password.txt
-#eosio-cpp -o roulette.wasm roulette.cpp --abigen
-#cleos set contract roulette ~/eos/CONTRACTS_DIR/roulette/ -p roulette@owner
-#cleos push action eosio.token transfer '{"from":"roulette","to":"alice","quantity":"0.0042 EOS","memo":"giving back to Alice"}' -p roulette@owner
-#cleos push action eosio.token transfer '{"from":"alice","to":"roulette","quantity":"1.0000 EOS","memo":"giving back to Alice"}' -p alice@owner
-#cleos push action roulette deleteall '["roulette"]' -p roulette@owner
+./common.sh
+cleos push action roulette deleteall '["roulette"]' -p roulette@owner
 
-echo Alice has
-cleos get currency balance eosio.token alice
+echo Available spins are
+cleos get table roulette roulette spins
+echo Standing bets are
+cleos get table roulette roulette bets
 echo Roulette has
 cleos get currency balance eosio.token roulette
-echo Spins are
-cleos get table roulette roulette spins
-echo Bets are
-cleos get table roulette roulette bets
+echo Alice has
+cleos get currency balance eosio.token alice
+echo Hit enter to create a spin
 read
 
 cleos push action roulette spin "[666, 1, $(date -d '+1 minute' +%s)]" -p roulette@owner
-echo Spins are
+echo Available spins are
 cleos get table roulette roulette spins
+echo Hit enter to bet on this spin
 read
 
-for i in {0..36}; do
-    cleos push action roulette bet "[\"alice\", 666, $i, 42, $RANDOM]" -p alice@owner
+for i in {0..1000}; do
+    cleos push action roulette bet "[\"alice\", 666, $((RANDOM % 37)), $((RANDOM % 10 + 1)), $RANDOM]" -p alice@owner
 done
-echo Bets are
+echo Standing bets are
 cleos get table roulette roulette bets
 echo Alice has
 cleos get currency balance eosio.token alice
 echo Roulette has
 cleos get currency balance eosio.token roulette
+echo Hit enter to pay winner
 read
 
 cleos push action roulette pay '[666]' -p roulette@owner
-echo Alice has
-cleos get currency balance eosio.token alice
+echo Hit enter to see final stats
+read
+
+echo Available spins are
+cleos get table roulette roulette spins
+echo Standing bets are
+cleos get table roulette roulette bets
 echo Roulette has
 cleos get currency balance eosio.token roulette
-echo Spins are
-cleos get table roulette roulette spins
-echo Bets are
-cleos get table roulette roulette bets
+echo Alice has
+cleos get currency balance eosio.token alice
+echo Thanks for playing
