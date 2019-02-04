927240db2a3cf9e0fa6cbcddf1b73896d545c032
israellevin <root@boltage>
2019-02-04 16:34:48 +0200

    Wait in loop for betting to end

--- test.sh
+++ test.sh
@@ -13,14 +13,14 @@ cleos get currency balance eosio.token alice
 echo Hit enter to create a spin
 read
 
-cleos push action roulette spin "[666, 1, $(date -d '+1 minute' +%s)]" -p roulette@owner
+cleos push action roulette spin "[777, 1, $(date -d '+5 seconds' +%s)]" -p roulette@owner
 echo Available spins are
 cleos get table roulette roulette spins
 echo Hit enter to bet on this spin
 read
 
 for i in {0..100}; do
-    cleos push action roulette bet "[\"alice\", 666, $((RANDOM % 37)), $((RANDOM % 10 + 1)), $RANDOM]" -p alice@owner
+    cleos push action roulette bet "[\"alice\", 777, $((RANDOM % 37)), $((RANDOM % 10 + 1)), $RANDOM]" -p alice@owner
 done
 echo Standing bets are
 cleos get table roulette roulette bets
@@ -31,7 +31,7 @@ cleos get currency balance eosio.token roulette
 echo Hit enter to pay winner
 read
 
-cleos push action roulette pay '[666]' -p roulette@owner
+while ! cleos push action roulette pay '[777]' -p roulette@owner; do date; done
 echo Hit enter to see final stats
 read
 
