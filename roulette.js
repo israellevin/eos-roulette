6dde19e87e7c7443459ee527b05d30e377dabd88
Israel Levin <israel@paket.global>
2019-02-06 01:29:44 +0200

    Break to functions

--- roulette.js
+++ roulette.js
@@ -4,16 +4,15 @@
 
     const privateKey = '5J8jB9ErRQP1yuiPEDTfseoSC6DMYZrXSZDkn2Ehw3Mje2rA3Z4';
     const rpc = new eosjs_jsonrpc.default('http://127.0.0.1:8888');
-    const signatureProvider = new eosjs_jssig.default([privateKey]);
-    const api = new eosjs_api.default({rpc, signatureProvider});
 
-    function getBalance(success, failure){
+    // Get a user's balance.
+    function getBalance(user, success, failure){
         (async() => {
             try{
                 const result = await rpc.get_table_rows({
                     json: true,
                     code: 'eosio.token',
-                    scope: 'alice',
+                    scope: user,
                     table: 'accounts',
                     limit: 10,
                 });
@@ -25,91 +24,107 @@
     }
     window.getBalance = getBalance;
 
-    function bet(towin, larimers, seed, success, failure){
+    const signatureProvider = new eosjs_jssig.default([privateKey]);
+    const api = new eosjs_api.default({rpc, signatureProvider});
+
+    async function deleteall(){
+        return await api.transact({
+            actions: [{
+                account: 'roulette',
+                name: 'deleteall',
+                authorization: [{
+                    actor: 'roulette',
+                    permission: 'owner',
+                }],
+                data: {},
+            }]
+        }, {
+            blocksBehind: 3,
+            expireSeconds: 30,
+        });
+    }
+
+    async function spin(seed_hash, min_bet_time, max_bet_time){
+        return await api.transact({
+            actions: [{
+                account: 'roulette',
+                name: 'spin',
+                authorization: [{
+                    actor: 'roulette',
+                    permission: 'owner',
+                }],
+                data: {
+                    seed_hash: seed_hash,
+                    min_bet_time: min_bet_time,
+                    max_bet_time: max_bet_time,
+                },
+            }]
+        }, {
+            blocksBehind: 3,
+            expireSeconds: 30,
+        });
+    }
+
+    async function bet(spinseedhash, towin, larimers, seed){
+        return await api.transact({
+            actions: [{
+                account: 'roulette',
+                name: 'bet',
+                authorization: [{
+                    actor: 'alice',
+                    permission: 'owner',
+                }],
+                data: {
+                    user: 'alice',
+                    spinseedhash: spinseedhash,
+                    towin: towin,
+                    larimers: larimers,
+                    seed: seed,
+                },
+            }]
+        }, {
+            blocksBehind: 3,
+            expireSeconds: 30,
+        });
+    }
+
+    async function pay(spinseed){
+        return await api.transact({
+            actions: [{
+                account: 'roulette',
+                name: 'pay',
+                authorization: [{
+                    actor: 'roulette',
+                    permission: 'owner',
+                }],
+                data: {
+                    spinseed: spinseed,
+                },
+            }]
+        }, {
+            blocksBehind: 3,
+            expireSeconds: 30,
+        });
+    }
+
+    window.bet = function(towin, larimers, seed, success, failure){
         (async() => {
-            let result;
             try{
-                result = await api.transact({
-                    actions: [{
-                        account: 'roulette',
-                        name: 'deleteall',
-                        authorization: [{
-                            actor: 'roulette',
-                            permission: 'owner',
-                        }],
-                        data: {},
-                    }]
-                }, {
-                    blocksBehind: 3,
-                    expireSeconds: 30,
-                });
+                console.log('del', await deleteall());
+                console.log('spn', await spin(888, 0, Math.round(new Date().getTime() / 1000) + 2));
+                console.log('bet', await bet(888, towin, larimers, seed));
 
-                result = await api.transact({
-                    actions: [{
-                        account: 'roulette',
-                        name: 'spin',
-                        authorization: [{
-                            actor: 'roulette',
-                            permission: 'owner',
-                        }],
-                        data: {
-                            seed_hash: 888,
-                            min_bet_time: 0,
-                            max_bet_time: Math.round(new Date().getTime() / 1000) + 2,
-                        },
-                    }]
-                }, {
-                    blocksBehind: 3,
-                    expireSeconds: 30,
-                });
-
-                result = await api.transact({
-                    actions: [{
-                        account: 'roulette',
-                        name: 'bet',
-                        authorization: [{
-                            actor: 'alice',
-                            permission: 'owner',
-                        }],
-                        data: {
-                            user: 'alice',
-                            spinseedhash: 888,
-                            towin: towin,
-                            larimers: larimers,
-                            seed: 1,
-                        },
-                    }]
-                }, {
-                    blocksBehind: 3,
-                    expireSeconds: 30,
-                });
-
+                alert('wheel is spinning');
                 await function sleep(ms){
                     return new Promise(resolve => setTimeout(resolve, ms));
-                }(2000);
+                }(3000);
 
-                result = await api.transact({
-                    actions: [{
-                        account: 'roulette',
-                        name: 'pay',
-                        authorization: [{
-                            actor: 'roulette',
-                            permission: 'owner',
-                        }],
-                        data: {
-                            spinseed: 888,
-                        },
-                    }]
-                }, {
-                    blocksBehind: 3,
-                    expireSeconds: 30,
-                });
-
-                success(result);
+                let payresult = await pay(888);
+                console.log('pay', payresult);
+                success(payresult.processed.action_traces[0].console);
             }catch(e){
                 failure(e.json);
             }
         })();
-    }
-    window.bet = bet;
+    };
 }());
