7238b19122f118ecb15dbdc73dbce8577ffe88d4
Israel Levin <israel@paket.global>
2019-02-10 11:31:45 +0200

    Work with hashes
    Seed and hash still identical.

--- test.sh
+++ test.sh
@@ -2,6 +2,9 @@
 ./common.sh
 cleos push action roulette deleteall '["roulette"]' -p roulette@owner
 
+seed=$(openssl rand -hex 32)
+seedhash="$(cleos push action -j roulette hash '["'$seed'"]' -p roulette@owner | grep -Po '(?<="console": ").*(?=\")')"
+
 echo Available spins are
 cleos get table roulette roulette spins
 echo Standing bets are
@@ -13,14 +16,14 @@ cleos get currency balance eosio.token alice
 echo Hit enter to create a spin
 read
 
-cleos push action roulette spin "[777, 1, $(date -d '+5 seconds' +%s)]" -p roulette@owner
+cleos push action roulette spin '["'$seedhash'", 1, '$(date -d '+5 seconds' +%s)']' -p roulette@owner
 echo Available spins are
 cleos get table roulette roulette spins
 echo Hit enter to bet on this spin
 read
 
-for i in {0..100}; do
-    cleos push action roulette bet "[\"alice\", 777, $((RANDOM % 37)), $((RANDOM % 10 + 1)), $RANDOM]" -p alice@owner
+for i in {0..10}; do
+    cleos push action roulette bet '["alice", "'$seedhash'", '$((RANDOM % 37))", $((RANDOM % 10 + 1)), $i]" -p alice@active
 done
 echo Standing bets are
 cleos get table roulette roulette bets
@@ -31,7 +34,7 @@ cleos get currency balance eosio.token roulette
 echo Hit enter to pay winner
 read
 
-while ! cleos push action roulette pay '[777]' -p roulette@owner; do sleep 1; done
+while ! cleos push action roulette pay '["'$seedhash'", "'$seed'"]' -p roulette@owner; do sleep 1; done
 echo Hit enter to see final stats
 read
 
