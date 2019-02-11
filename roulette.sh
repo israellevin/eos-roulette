da36a7ae0f5e8c3af8ad57b2598c0fa7242f1fbe
Israel Levin <israel@paket.global>
2019-02-11 16:08:13 +0200

    Server saves seeds

--- roulette.sh
+++ roulette.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 ./common.sh
+seedsdir=seeds
+mkdir -p "$seedsdir"
+
+pay(){
+    cleos push action roulette pay '["'$(cat "$seedsdir/$1")'"]' -p roulette@owner
+}
+
+spin(){
+    seed=$(openssl rand -hex 32)
+    seedhash=$(cleos push action -j roulette gethash '["'$seed'"]' -p roulette@owner | grep -Po '(?<="console": ").*(?=\")')
+    echo $seed > seeds/$seedhash
+    cleos push action roulette spin '["'$seedhash'", '$(date +%s)', '$1']' -p roulette@owner
+}
+
 cleos get table roulette roulette spins
 while :; do
-    oldest=$(cleos get table roulette roulette spins --index 2 --key-type i64 -U $(date +%s) -l1 | sed -n '3s/.*: \(.*\),/\1/gp')
+    oldest=$(cleos get table roulette roulette spins --index 3 --key-type i64 -U $(date +%s) -l1 | grep -Po '(?<="seedhash": ").*(?=\")')
     [ "$oldest" ] || break;
-    cleos push action roulette pay '["'$oldest'", "'$oldest'"]' -p roulette@owner
+    pay $oldest
 done
-current=$(cleos get table roulette roulette spins --index 2 --key-type i64 -L $(date +%s) -l1 | sed -n '3s/.*: \(.*\),/\1/gp')
+current=$(cleos get table roulette roulette spins --index 3 --key-type i64 -L $(date +%s) -l1 | grep -Po '(?<="seedhash": ").*(?=\")')
 for i in {1..5}; do
-    cleos push action roulette spin "[$RANDOM, 1, $(date -d "+$((RANDOM % 100 + 30)) second" +%s)]" -p roulette@owner
+    spin $(date -d "+$((RANDOM % 300 + 60)) second" +%s)
 done
 cleos get table roulette roulette spins
