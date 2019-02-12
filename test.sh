837780fef1d5205c5a7cbbdc2f276c3fa6644fe7
Israel Levin <israel@paket.global>
2019-02-12 14:29:00 +0200

    Some testing for winner calculation

--- test.sh
+++ test.sh
@@ -2,15 +2,6 @@
 ./common.sh
 cleos push action roulette deleteall '["roulette"]' -p roulette@owner
 
-#for i in {1..999}; do
-#    echo $i 1>&2
-#    seed=$(openssl rand -hex 32)
-#    winner="$(cleos push action -j roulette getwinner '["'$seed'"]' -p roulette@owner | grep -Po '(?<="console": ").*(?=\")')"
-#    echo $winner
-#done | sort -n | uniq -c
-#
-#exit 1
-
 seed=$(openssl rand -hex 32)
 seedhash="$(cleos push action -j roulette gethash '["'$seed'"]' -p roulette@owner | grep -Po '(?<="console": ").*(?=\")')"
 
