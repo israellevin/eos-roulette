addcc097cc176dbdca1d1a6a0b1c5d1941d1c538
Israel <root@boltage>
2019-02-14 01:21:36 +0200

    Better printouts

--- roulette.sh
+++ roulette.sh
@@ -2,29 +2,41 @@
 ./common.sh
 seedsdir=seeds
 mkdir -p "$seedsdir"
+paid=()
+spun=()
 
 pay(){
-    cleos push action roulette pay '["'$(cat "$seedsdir/$1")'"]' -p roulette@owner
+    paid+=("$(cleos push action roulette pay '["'$(cat "$seedsdir/$1")'"]' -p roulette@owner)")
 }
 
 spin(){
     seed=$(openssl rand -hex 32)
     seedhash=$(cleos push action -j roulette gethash '["'$seed'"]' -p roulette@owner | grep -Po '(?<="console": ").*(?=\")')
     echo $seed > seeds/$seedhash
-    cleos push action roulette spin '["'$seedhash'", '$(date +%s)', '$1']' -p roulette@owner
+    spun+=("$(cleos push action roulette spin '["'$seedhash'", '$(date +%s)', '$1']' -p roulette@owner)")
 }
 
-echo 'open spins:' `cleos get table roulette roulette spins -l -1 | grep id\" | wc -l`
-cleos get table roulette roulette spins -l 20| grep id\"
-paid=0
+
+echo $(cleos get table roulette roulette spins -l999 | grep -o '^ *"id": [[:digit:]]*,$' | wc -l) spins
+
 while :; do
-    oldest=$(cleos get table roulette roulette spins --index 3 --key-type i64 -U $(date +%s) -l1 | grep -Po '(?<="seedhash": ").*(?=\")')
-    [ "$oldest" ] || break;
-    pay $oldest
-    let paid++
+    payable=$(\
+        cleos get table roulette roulette spins --index 3 --key-type i64 -U $(date +%s) -l1 |\
+        grep -Po '(?<="seedhash": ").*(?=\")')
+    [ "$payable" ] || break;
+    pay $payable 2>/dev/null
 done
-echo 'paid' $((paid)) 'spins'
-current=$(cleos get table roulette roulette spins --index 3 --key-type i64 -L $(date +%s) -l1 | grep -Po '(?<="seedhash": ").*(?=\")')
+
+echo ${#paid[@]} paid
+echo $(cleos get table roulette roulette spins -l999 | grep -o '^ *"id": [[:digit:]]*,$' | wc -l) spins
+
 for i in $(seq 5 5 30); do
-    spin $(date -d "+$i second" +%s)
+    spin $(date -d "+$i second" +%s) 2>/dev/null
 done
+
+
+echo ${#spun[@]} spun
+echo $(cleos get table roulette roulette spins -l999 | grep -o '^ *"id": [[:digit:]]*,$' | wc -l) spins
+
+printf '%s\n' "${paid[@]}"
+printf '%s\n' "${spun[@]}"
