8e2df748cf6d9fa594f5ca1f93754a2efc2d116e
Israel <root@boltage>
2019-02-16 17:10:31 +0200

    Initialize a fuller system with data limits
    Also get rid of unnecessary curly braces.

--- deploy.sh
+++ deploy.sh
@@ -15,20 +15,24 @@ echo '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3' | cleos wallet import
 
 # Create accounts.
 pubkey=$(cat ./pubkey.txt)
-cleos create account eosio eosio.token ${pubkey} -p eosio@active
-cleos create account eosio eosio.system ${pubkey} -p eosio@active
-cleos create account eosio roulette ${pubkey} -p eosio@active
-cleos create account eosio alice ${pubkey} -p eosio@active
+for account in eosio.bpay eosio.msig eosio.names eosio.ram eosio.ramfee eosio.saving eosio.stake eosio.token eosio.upay roulette alice; do
+    cleos create account eosio $account $pubkey -p eosio@active
+done
 
-# Get and compile token contract.
+# Get and compile contracts.
 git clone https://github.com/eosio/eosio.contracts
+eosio-cpp -I eosio.contracts/eosio.bios/include/ -o eosio.bios.wasm eosio.contracts/eosio.bios/src/eosio.bios.cpp --abigen
+mv eosio.bios.wasm eosio.bios.abi eosio.contracts/eosio.bios/.
+cleos set contract eosio ./eosio.contracts/eosio.bios/ -p eosio@active
+
 eosio-cpp -I eosio.contracts/eosio.token/include/ -o eosio.token.wasm eosio.contracts/eosio.token/src/eosio.token.cpp --abigen
 mv eosio.token.wasm eosio.token.abi eosio.contracts/eosio.token/.
 cleos set contract eosio.token ./eosio.contracts/eosio.token/ -p eosio.token@active
 
-#compile system token
-eosio-cpp -I eosio.contracts/eosio.system/include/ -I eosio.contracts/eosio.token/include/ -o eosio.system.wasm eosio.contracts/eosio.system/src/eosio.system.cpp --abigen
+# Note that this seems to override the bios, as seen from `cleos get code eosio`.
+eosio-cpp -I eosio.contracts/eosio.token/include/ -I eosio.contracts/eosio.system/include/ -o eosio.system.wasm eosio.contracts/eosio.system/src/eosio.system.cpp --abigen
 mv eosio.system.wasm eosio.system.abi eosio.contracts/eosio.system/.
+cleos set contract eosio ./eosio.contracts/eosio.system/ -p eosio@active
 
 # Create token and distribute.
 cleos push action eosio.token create '["eosio", "10000000000.0000 EOS"]' -p eosio.token@active
@@ -36,11 +40,19 @@ cleos push action eosio.token issue '["eosio", "10000000000.0000 EOS", "issue eo
 cleos push action eosio.token transfer '{"from":"eosio","to":"roulette","quantity":"10000.0000 EOS","memo":"funding roulette"}' -p eosio@active
 cleos push action eosio.token transfer '{"from":"eosio","to":"alice","quantity":"10000.0000 EOS","memo":"funding alice"}' -p eosio@active
 
+# This init call is required, and can only happen after token is issued.
+cleos push action eosio init '[0, "4,EOS"]' -p eosio@active
+
+# Get RAM and BW.
+cleos system buyram alice alice '1.0000 EOS'
+cleos system delegatebw alice alice '1.0000 EOS' '1.0000 EOS' -p alice@active
+
+# Compile and set the roulette contract.
 ./compile.sh
 
 # Give permissions.
-cleos set account permission roulette active '{"threshold":1,"keys":[{"key":"'${pubkey}'","weight":1}],"accounts":[{"permission":{"actor":"roulette","permission":"eosio.code"},"weight":1}]}' owner -p roulette@owner
-cleos set account permission alice active '{"threshold":1,"keys":[{"key":"'${pubkey}'","weight":1}],"accounts":[{"permission":{"actor":"roulette","permission":"eosio.code"},"weight":1}]}' owner -p alice@owner
+cleos set account permission roulette active '{"threshold":1,"keys":[{"key":"'$pubkey'","weight":1}],"accounts":[{"permission":{"actor":"roulette","permission":"eosio.code"},"weight":1}]}' owner -p roulette@owner
+cleos set account permission alice active '{"threshold":1,"keys":[{"key":"'$pubkey'","weight":1}],"accounts":[{"permission":{"actor":"roulette","permission":"eosio.code"},"weight":1}]}' owner -p alice@owner
 
 # Put the chain ID in js file, for convenience.
 echo "window.roulette = {chainid: '$(cat chainid.txt)'};" > js/eosjs-chainid.js
--- roulette.sh
+++ roulette.sh
@@ -11,9 +11,9 @@ pay(){
 
 spin(){
     seed=$(openssl rand -hex 32)
-    seedhash=$(cleos push action -j roulette gethash '["'${seed}'"]' -p roulette@owner | grep -Po '(?<="console": ").*(?=\")')
-    echo ${seed} > seeds/${seedhash}
-    spun+=("$(cleos push action roulette spin '["'${seedhash}'", '$(date +%s)', '$1']' -p roulette@owner)")
+    seedhash=$(cleos push action -j roulette gethash '["'$seed'"]' -p roulette@owner | grep -Po '(?<="console": ").*(?=\")')
+    echo $seed > seeds/$seedhash
+    spun+=("$(cleos push action roulette spin '["'$seedhash'", '$(date +%s)', '$1']' -p roulette@owner)")
 }
 
 
@@ -24,7 +24,7 @@ while :; do
         cleos get table roulette roulette spins --index 3 --key-type i64 -U $(date +%s) -l1 |\
         grep -Po '(?<="seedhash": ").*(?=\")')
     [ "$payable" ] || break;
-    pay ${payable} 2>/dev/null
+    pay $payable 2>/dev/null
 done
 
 echo ${#paid[@]} paid
